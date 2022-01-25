52440096204b750a4b0b717ef7fc820511795395
Pararth Dave <pararthdave@gmail.com>
2022-01-25 12:43:39 +0530

    set and dict

---
+++ Practice/dict.py
@@ -0,0 +1,23 @@
+dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(dict)
+print("Brand: ",dict['brand']) #on calling key value is returned, get function can also be used to perform this task
+
+family = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(family['child1'])  #nested dictionary printing    
---
+++ Practice/set.py
@@ -0,0 +1,3 @@
+st={1,2,3}
+print(st)
+#print(st[0]) will give error as set are unindexed  
